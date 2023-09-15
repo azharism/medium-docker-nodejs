d18fce8c01ac222b05aafe6234478bea24c7b3b2
azhar <87310169+azharism@users.noreply.github.com>
2023-09-15 22:33:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 4000;
 
 
 app.get('/', (req, res) => {
-	res.send("Hello world!");
+	res.send("Welcom to azhar world");
 })
 
 app.listen(PORT, () => console.log("App listening at port " + PORT));
